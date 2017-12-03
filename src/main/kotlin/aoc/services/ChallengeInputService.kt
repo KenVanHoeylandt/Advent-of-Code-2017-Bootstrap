8a6f5c037fa91842d77a784fac533ef6691068d7
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2017-12-03 23:06:36 +0100

    updated for Kotlin 1.2.0 + improvements on ChallengeInputService

--- src/main/kotlin/aoc/services/ChallengeInputService.kt
+++ src/main/kotlin/aoc/services/ChallengeInputService.kt
@@ -32,7 +32,7 @@ class ChallengeInputService(private val requestService: RequestService) {
 	private fun getInputFromLocalFile(day: Int): Single<String> {
 		return Single.just(day)
 				.observeOn(Schedulers.io())
-				.map { getLocalFile(it) }
+				.map(::getLocalFile)
 				.map(File::readBytes)
 				.map { String(it) }
 	}
@@ -43,58 +43,64 @@ class ChallengeInputService(private val requestService: RequestService) {
 	 * @return the data as a String
 	 */
 	private fun getInputFromWebsite(day: Int): Single<String> {
+		val url = getUrl(day)
 		val request = requestService.requestBuilder()
-				.url("https://adventofcode.com/${Configuration.year}/day/$day/input")
+				.url(url)
 				.build()
 
 		return requestService.executeForString(request)
-				.map { stripNewline(it) }
+				.map(::stripNewline)
 				.doOnSuccess { storeInputToFile(it, day) }
 	}
+}
 
-	/**
-	 * Strips the last character if it is a newline character and returns the resulting String.
-	 */
-	private fun stripNewline(text: String): String {
-		return if (text.endsWith('\n')) {
-			text.substring(0, text.length - 1)
-		} else {
-			text
-		}
-	}
-
-	/**
-	 * Store the text input for the specified day.
-	 */
-	private fun storeInputToFile(text: String, day: Int) {
-		// Write data to local cache
-		val file = getLocalFile(day)
-
-		if (!file.exists()) {
-			file.createNewFile()
-		}
-
-		// Assignment data is not very big, so we can write it unbuffered
-		val outputStream = FileOutputStream(file)
-		outputStream.write(text.toByteArray())
-		outputStream.close()
-	}
-
-	/**
-	 * Get the local file to store the local data.
-	 * The file might not exist yet.
-
-	 * @param day the day to retrieve File for
-	 * *
-	 * @return the file (which might not exist yet)
-	 */
-	private fun getLocalFile(day: Int): File {
-		val directory = File("data")
-
-		if (!directory.isDirectory) {
-			directory.mkdir()
-		}
-
-		return File(directory, "day$day")
+/**
+ * Strips the last character if it is a newline character and returns the resulting String.
+ */
+private fun stripNewline(text: String): String {
+	return if (text.endsWith('\n')) {
+		text.substring(0, text.length - 1)
+	} else {
+		text
 	}
 }
+
+/**
+ * Store the text input for the specified day.
+ */
+private fun storeInputToFile(text: String, day: Int) {
+	// Write data to local cache
+	val file = getLocalFile(day)
+
+	if (!file.exists()) {
+		file.createNewFile()
+	}
+
+	// Assignment data is not very big, so we can write it unbuffered
+	val outputStream = FileOutputStream(file)
+	outputStream.write(text.toByteArray())
+	outputStream.close()
+}
+
+/**
+ * Get the local file to store the local data.
+ * The file might not exist yet.
+
+ * @param day the day to retrieve File for
+ * *
+ * @return the file (which might not exist yet)
+ */
+private fun getLocalFile(day: Int): File {
+	val directory = File("data")
+	val filename = getFilename(day)
+
+	if (!directory.isDirectory) {
+		directory.mkdir()
+	}
+
+	return File(directory, filename)
+}
+
+private fun getFilename(day: Int): String = "${Configuration.year} day $day.txt"
+
+private fun getUrl(day: Int) = "https://adventofcode.com/${Configuration.year}/day/$day/input"
